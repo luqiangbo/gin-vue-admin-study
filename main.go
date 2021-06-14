78c3e014ffb0968aa686429ed959b26949645df4
caniuse <luqiangbovip@163.com>
2021-06-14 16:11:34 +0800

    新建表常用设置

--- main.go
+++ main.go
@@ -5,12 +5,13 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
-type TableHello struct {
+type User struct {
 	gorm.Model
-	Name   string
-	Sex    bool
-	Age    int
-	Friend string
+	Name string `gorm:"primary_key;column:user_name;type:varchar(100)"`
+}
+
+func (u User) TableName() string {
+	return "table_users"
 }
 
 func main() {
@@ -19,56 +20,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	db.AutoMigrate(&TableHello{})
-
-	// 创建
-	//db.Create(&TableHello{
-	//	Name:   "lqb3",
-	//	Sex:    false,
-	//	Age:    89,
-	//	Friend: "xiaoming",
-	//})
-
-	// 查找一个
-	//var hello TableHello
-	//db.First(&hello)
-
-	// 查找多个
-	//var hello []TableHello
-	//db.Find(&hello)
-
-	// 查询条件
-	//var hello []TableHello
-	//db.Where("age < ?",21).Find(&hello)
-
-	// 更新1
-	//db.Where("id = ?", 1).First(&TableHello{}).Update("name", "小二")
-
-	// 更新 2
-	//db.Where("id = ?", 1).First(&TableHello{}).Updates(TableHello{
-	//	Name: "更新2",
-	//	Age:  2,
-	//})
-
-	//更新 3
-	//db.Where("id = ?", 1).First(&TableHello{}).Updates(map[string]interface{}{
-	//	"Name": "更新31",
-	//	"Sex":  false,
-	//	"Age":  31,
-	//})
-
-	//更新 4
-	//db.Where("id in (?)", []int{1, 2}).First(&[]TableHello{}).Updates(map[string]interface{}{
-	//	"Name": "批量更新",
-	//	"Sex":  false,
-	//	"Age":  322,
-	//})
-
-	// 删除 软
-	//db.Where("id in (?) ", []int{1, 2}).Delete(&TableHello{})
-
-	// 删除 硬
-	db.Where("id in (?) ", []int{1, 2}).Unscoped().Delete(&TableHello{})
+	db.AutoMigrate(&User{})
 
 	//fmt.Println(hello)
 
